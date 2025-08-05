f73b68dc0330ce4674dd0b3c0c562403dc31bea4
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-08-05 14:16:18 -0400

    Create defaultList function
    Creates the defaultList function which allows for the initialization of a dummy list on webpage startup.

---
+++ src/defaultList.js
@@ -0,0 +1,16 @@
+import { listArray } from "./listCreator";
+import Task from "./taskcreator";
+
+export default function defaultListCreation() {
+const sampleTask = new Task("Grab eggs", "04/23");
+sampleTask.setDescription("Make sure to grab organic eggs.")
+listArray.push(sampleTask);
+
+const sampleTask2 = new Task("Grab milk", "04/23");
+sampleTask2.setDescription("Make sure NOT to grab organic milk!");
+listArray.push(sampleTask2);
+
+const sampleTask3 = new Task("Grab fish", "04/23");
+sampleTask3.setDescription("Always snapper.")
+listArray.push(sampleTask3);
+}
